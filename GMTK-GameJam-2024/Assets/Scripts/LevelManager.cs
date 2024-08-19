a662b65e7f01cbea226cd53ef868cc7c261f1956
Luisa Velandia <75763159+l-vel@users.noreply.github.com>
2024-08-18 21:26:11 -0500

    finishLevel block appears at the end of each level

--- GMTK-GameJam-2024/Assets/Scripts/LevelManager.cs
+++ GMTK-GameJam-2024/Assets/Scripts/LevelManager.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
-
-    
+    public GameObject finishLevelPrefab;
+    public ObstacleCreator obstacleCreator;
+    private Vector3 levelHeight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnFinishPoint();
     }
 
     // Update is called once per frame
@@ -18,6 +19,12 @@ public class LevelManager : MonoBehaviour
         
     }
 
+
+    private void spawnFinishPoint()
+    {
+        levelHeight = new Vector3(0,obstacleCreator.levelHeight,0);
+        Instantiate(finishLevelPrefab, levelHeight, Quaternion.identity);
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Finish" )
